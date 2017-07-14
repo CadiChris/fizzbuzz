e27e101efe04306f66d250e9d1a0a946f252dd86
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2017-07-14 19:57:56 +0200

    Formatting

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,13 +2,10 @@ const fizzbuzz = (chiffre) => {
 
     const estModuloDe = (modulo) => chiffre % modulo === 0
 
-    return estModuloDe(3) && estModuloDe(5)
-        ? 'FizzBuzz'
-        : estModuloDe(3)
-            ? 'Fizz'
-            : estModuloDe(5)
-                ? 'Buzz'
-                : chiffre
+    return estModuloDe(3) && estModuloDe(5) ? 'FizzBuzz'
+        : estModuloDe(3) ? 'Fizz'
+        : estModuloDe(5) ? 'Buzz'
+        : chiffre
 }
 
 
